cbe4960bb810d40296c76d5379008c1a136b9020
quoc-toan07 <tran.quoc.toan7599@gmail.com>
2026-09-22 15:55:22 +0700

    Hoan thanh bai2 lab2 & video

--- Lab2/Bai2_TinhDiem.c
+++ Lab2/Bai2_TinhDiem.c
@@ -2,16 +2,17 @@
 int main()
 {
   float toan, ly, hoa;
-  printf("Nhap diem Toan:");
+
+  printf("Nhap diem Toan: ");
   scanf("%f", &toan);
 
-  printf("Nhap diem Ly:");
+  printf("Nhap diem Ly: ");
   scanf("%f", &ly);
 
-  printf("Nhap diem Hoa:");
+  printf("Nhap diem Hoa: ");
   scanf("%f", &hoa);
 
-  float dtb = ((toan * 3) + (ly * 2) + (hoa * 1)) / 6;
+  float dtb = ((toan * 3) + (ly * 2) + (hoa * 1)) / (float)6;
 
   printf("Diem trung binh: %.2f", dtb);
 }
--- Lab2/Video_Lab2.md
+++ Lab2/Video_Lab2.md
@@ -1,6 +1,6 @@
 ### Tổng hợp video bài tập Lab1
 
 - Bài 1: https://youtu.be/fAZoNbm6dc0
-- Bài 2:
+- Bài 2: https://youtu.be/ETSCCS6OrFg
 - Bài 3:
 - Bài 4:
